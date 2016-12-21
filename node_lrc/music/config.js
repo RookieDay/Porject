f314611ff3eae7cf41a78e745a57919ebb2ebf71
RookieDay <1109318841@qq.com>
2016-12-22 01:02:35 +0800

    fix

--- node_lrc/music/config.js
+++ node_lrc/music/config.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
     port: 3000,
     host: '127.0.0.1',
-    templateFilePath: path.join(__dirname, 'views');
+    templateFilePath: path.join(__dirname, 'views'),
     musicPath: __dirname
 }
