dcb4fdc4195bff8615ae1a843632c0e2b7d10bdd
RookieDay <1109318841@qq.com>
2016-12-24 14:53:09 +0800

    node music

---
+++ node_music/controllers/music.js
@@ -0,0 +1,133 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+const querystring = require('querystring');
+const musicPath = require('../config').musicPath;
+const musicList = require('../models/music');
+const Music = musicList;
+
+exports.renderIndex = (req, res) => {
+    res.render('index');
+}
+
+exports.getMusicList = (req, res) => {
+    Music.getAll((err, rows) => {
+        if (err) {
+            return res.end(err.message);
+        }
+        res.json({
+            musicList: rows
+        })
+    })
+}
+
+exports.transferMusic = (req, res) => {
+    let fullPath = path.join(musicPath, req.pathname);
+    let readStream = fs.createReadStream(fullPath);
+
+    readStream.pipe(res);
+}
+
+exports.renderAdd = (req, res) => {
+    res.render('add');
+}
+
+exports.doAdd = (req, res) => {
+    let data = '';
+    req.on('data', (chunk) => {
+        data += chunk;
+    })
+    req.on('end', () => {
+        let obj = querystring.parse(data);
+        let music = new Music(obj);
+        music.save((err, rows) => {
+            if (err) {
+                return res.end(err.message);
+            }
+            res.writeHead(302, {
+                'Location': 'http://127.0.0.1:3000/'
+            })
+            res.end();
+        })
+    })
+}
+
+
+exports.renderEdit = (req, res) => {
+    let mid = req.query.mid;
+    Music.getColumn(mid, (err, rows) => {
+        if (err) {
+            return res.json({
+                code: '5003',
+                msg: 'music not found'
+            })
+        }
+        console.log(rows);
+        let music = rows;
+        res.render('edit', {
+            music: rows[0]
+        })
+    })
+}
+
+exports.doEdit = (req, res) => {
+    let mid = req.query.mid;
+    // let index = musicList.findIndex(m => m.id === mid);
+    Music.getColumn(mid, (err, rows) => {
+        if (err) {
+            return res.json({
+                code: '5002',
+                msg: 'music info not found'
+            })
+        }
+        let selectTitle = rows[0].title;
+
+        let data = '';
+        req.on('data', (chunk) => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            data = querystring.parse(data);
+            data.id = mid;
+            let music = new Music(data);
+            music.id = mid;
+            music.update((err, rows) => {
+                if (err) {
+                    return res.json({
+                        code: '5003',
+                        msg: 'music updatea failed'
+                    })
+                }
+                res.writeHead(302, {
+                    'Location': 'http://127.0.0.1:3000/'
+                })
+                res.end();
+            })
+        })
+
+    })
+
+}
+exports.doRemove = (req, res) => {
+    let mid = req.query.mid;
+    // let index = musicList.findIndex(m => m.id === mid);
+    Music.getColumn(mid, (err, rows) => {
+        if (err) {
+            return res.json({
+                code: '6002',
+                msg: 'music info not found'
+            })
+        }
+        Music.remove(mid, (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            res.json({
+                code: '6000',
+                msg: 'remove success'
+            })
+        });
+    })
+
+
+}
---
+++ node_music/models/music.js
@@ -0,0 +1,61 @@
+'use strict';
+const db = require('./db');
+
+function Music(music) {
+    this.title = music.title;
+    this.time = music.time;
+    this.singer = music.singer;
+    this.src = music.src;
+}
+Music.getAll = function(callback) {
+    db.query('select * from music', (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        // console.log(rows);
+        callback(null, rows);
+    })
+}
+Music.getColumn = function(id, callback) {
+    db.query(`
+        select * from music 
+        where id=${id}
+        `, (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        // console.log(rows);
+        callback(null, rows);
+    })
+}
+
+Music.remove = function(id, callback) {
+    db.query(`
+        DELETE from music
+        WHERE id="${id}"
+        `, (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, rows);
+    })
+}
+Music.prototype.save = function(callback) {
+    db.query(`
+    insert into music 
+    values(null,"${this.title}","${this.time}","${this.singer}","${this.src}")`, (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, rows);
+    })
+}
+Music.prototype.update = function(callback) {
+    db.query(`update music set title="${this.title}" where id="${this.id}"`, (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, rows);
+    })
+}
+module.exports = Music;
