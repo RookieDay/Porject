8e8e1bd9bc51d19949fbf1d70796ea3fd3090da4
RookieDay <1109318841@qq.com>
2016-12-27 00:04:21 +0800

    tempsave

--- 基础狂击/瀑布流Ajax版/js/jquery.waterFall.js
+++ 基础狂击/瀑布流Ajax版/js/jquery.waterFall.js
@@ -1,4 +1,26 @@
 ;
 (function($) {
+    $.fn.waterFall = function(options) {
+        // 合并默认值 参数
+        var defaults = $.extend({
+            gap: 20
+        }, options);
+
+        //初始化
+        var _this = $(this);
+    }
 
 })(jQuery);
+
+
+// var obj = {
+//     name: 'itcast',
+//     age: 10
+// }
+// var opt = {
+//     name: 'web',
+//     age: 15,
+//     sex: '男'
+// }
+// // 合并两个对象，后面的 有则替换 无则添加
+// console.log($.extend(obj, opt));
