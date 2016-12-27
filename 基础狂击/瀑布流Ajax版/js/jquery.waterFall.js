4a97f8cd18cd4bb0d52ea31f58eaabb9546491d4
RookieDay <1109318841@qq.com>
2016-12-27 21:50:05 +0800

    done

--- 基础狂击/瀑布流Ajax版/js/jquery.waterFall.js
+++ 基础狂击/瀑布流Ajax版/js/jquery.waterFall.js
@@ -7,7 +7,49 @@
         }, options);
 
         //初始化
-        var _this = $(this);
+        var _this = $(this),
+            items = _this.children(),
+            width = items.width(),
+            height = 0,
+            count = Math.floor(_this.width() / (width + defaults.gap)),
+            columns = [];
+        item.each(function(key, val) {
+            // 计算每个元素的高度
+            height = $(val).height();
+            // 第一行
+            if (key < count) {
+                // 每一列高度
+                column[key] = height;
+                // 设置定位坐标
+                $(val).css({
+                    top: 0,
+                    left: (width + defaults.gap)
+                })
+            } else {
+                var min_h = columns[0];
+                var min_k = 0;
+                // 取出最小列以及下标
+                for (var i = 0; i < columns.length; i++) {
+                    if (columns[i] < min_h) {
+                        min_h = columns[i];
+                        min_k = i;
+                    }
+                }
+                // 更新当前列的高度
+                columns[min_k] += height;
+                $(val).css({
+                    top: min_h + defaults.gap,
+                    left: (width + defaults.gap)
+                })
+            }
+        })
+
+        columns = columns.sort(function(a, b) {
+            return b - a;
+        })
+        _this.css({
+            height: columns[0]
+        })
     }
 
 })(jQuery);
