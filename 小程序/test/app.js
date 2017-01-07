ca6d3034b8555448c3517f10fc8e1160169cbaf7
RookieDay <1109318841@qq.com>
2017-01-08 00:43:44 +0800

    demo runned

--- 小程序/test/app.js
+++ 小程序/test/app.js
@@ -2,6 +2,7 @@
 //app.js是小程序的脚本代码。我们可以在这个文件中监听并处理小程序的生命周期函数、声明全局变量。
 //调用框架提供的丰富的 API，如本例的同步存储及同步读取本地数据。
 App({
+    //生命周期函数--监听小程序初始化  当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
     onLaunch: function() {
         //调用API从本地缓存中获取数据
         var logs = wx.getStorageSync('logs') || []
@@ -26,7 +27,54 @@ App({
             })
         }
     },
+    // 在 JavaScript 文件中声明的变量和函数只在该文件中有效； 不同的文件中可以声明相同名字的变量和函数， 
+    // 不会互相影响。
+    // 通过全局函数 getApp() 可以获取全局的应用实例， 如果需要全局的数据可以在 App() 中设置
     globalData: {
         userInfo: null
+    },
+    // 生命周期函数--监听小程序显示  当小程序启动，或从后台进入前台显示，会触发 onShow
+    onshow: function() {
+
+    },
+    // 生命周期函数--监听小程序隐藏 当小程序从前台进入后台，会触发 onHide
+    onhide: function() {
+
+    },
+    // 错误监听函数 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+    onerror: function() {
+
     }
 })
+
+// 前台、后台定义： 当用户点击左上角关闭，或者按了设备 Home 键离开微信，小程序并没有直接销毁，
+// 而是进入了后台；当再次进入微信或再次打开小程序，又会从后台进入前台。
+
+// 只有当小程序进入后台一定时间，或者系统资源占用过高，才会被真正的销毁。
+
+// App({
+//   onLaunch: function() { 
+//     // Do something initial when launch.
+//   },
+//   onShow: function() {
+//       // Do something when show.
+//   },
+//   onHide: function() {
+//       // Do something when hide.
+//   },
+//   onError: function(msg) {
+//     console.log(msg)
+//   },
+//   globalData: 'I am global data'
+// })
+
+
+// 注意：
+
+// App() 必须在 app.js 中注册，且不能注册多个。
+
+// 不要在定义于 App() 内的函数中调用 getApp() ，使用 this 就可以拿到 app 实例。
+
+// 不要在 onLaunch 的时候调用 getCurrentPage()，此时 page 还没有生成。
+
+// 通过 getApp() 获取实例之后，不要私自调用生命周期函数。
--- 小程序/test/pages/index/index.js
+++ 小程序/test/pages/index/index.js
@@ -1,26 +1,80 @@
 //index.js
-//获取应用实例
+//获取应用实例 我们提供了全局的 getApp() 函数，可以获取到小程序实例。
 var app = getApp()
+console.log(app.globalData.userInfo)
+    // Page() 函数用来注册一个页面。接受一个 object 参数，其指定页面的初始数据、生命周期函数、事件处理函数等。
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    // 页面的初始数据
+    data: {
+        motto: 'Hello World',
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    // 生命周期函数--监听页面加载
+    onLoad: function() {
+        console.log('onLoad')
+        var that = this
+            //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+    }
 })
+
+
+//index.js
+// Page({
+// 页面的初始数据
+//   data: {
+//     text: "This is page data."
+//   },
+// 生命周期函数--监听页面加载
+//   onLoad: function(options) {
+//     // Do some initialize when page load.
+//   },
+// 生命周期函数--监听页面初次渲染完成
+//   onReady: function() {
+//     // Do something when page ready.
+//   },
+// 生命周期函数--监听页面显示
+//   onShow: function() {
+//     // Do something when page show.
+//   },
+// 生命周期函数--监听页面隐藏
+//   onHide: function() {
+//     // Do something when page hide.
+//   },
+// 生命周期函数--监听页面卸载
+//   onUnload: function() {
+//     // Do something when page close.
+//   },
+// 页面相关事件处理函数--监听用户下拉动作
+//   onPullDownRefresh: function() {
+//     // Do something when pull down.
+//   },
+// 页面上拉触底事件的处理函数
+//   onReachBottom: function() {
+//     // Do something when page reach bottom.
+//   },
+// 用户点击右上角分享
+//   onShareAppMessage: function () {
+//    // return custom share data when user share.
+//   },
+//   // Event handler.
+//   viewTap: function() {
+//     this.setData({
+//       text: 'Set some data for updating view.'
+//     })
+//   },
+//   customData: {
+//     hi: 'MINA'
+//   }
+// })
