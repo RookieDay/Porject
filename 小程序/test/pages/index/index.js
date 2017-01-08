d8b9d2c73c5df93c9b72711310b5e655ecc8d27a
RookieDay <1109318841@qq.com>
2017-01-09 00:11:35 +0800

    view

--- 小程序/test/pages/index/index.js
+++ 小程序/test/pages/index/index.js
@@ -22,7 +22,16 @@ Page({
             index: 0,
             msg: 'this is a template',
             time: '2016-09-15'
-        }
+        },
+        imgUrls: [
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: false,
+        interval: 5000,
+        duration: 1000
     },
     //事件处理函数
     bindViewTap: function() {
@@ -86,6 +95,31 @@ Page({
         // console.log(event.target.dataset.alphaBeta === 1);
         // console.log(event.target.dataset.alphabeta === 2);
         console.log(event);
+    },
+    changeIndicatorDots: function(e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+    changeAutoplay: function(e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange: function(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange: function(e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+    changeCircular: function(e) {
+        this.setData({
+            circular: !this.data.circular
+        })
     }
 })
 
