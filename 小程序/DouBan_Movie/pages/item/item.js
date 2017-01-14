e92afd5d062a2c6ac91a877b448d5c637f808339
RookieDay <1109318841@qq.com>
2017-01-14 22:12:12 +0800

    update

--- 小程序/DouBan_Movie/pages/item/item.js
+++ 小程序/DouBan_Movie/pages/item/item.js
@@ -3,43 +3,43 @@
 var app = getApp();
 
 Page({
-  data: {
-    title: '',
-    loading: true,
-    movie: {}
-  },
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    var _this = this;
-    app.douban.findOne(options.id).then(function (d) {
-      return _this.setData({ title: d.title, movie: d, loading: false })
-    }).catch(function (e) {
-      _this.setData({ title: '获取数据异常', movie: {}, loading: false });
-      console.error(e);
-    })
-  },
-  onReady: function () {
-    // 页面渲染完成
-    wx.setNavigationBarTitle({
-      title: this.data.title + ' < 电影 < 豆瓣'
-    })
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  },
-  onShareAppMessage:function onShareAppMessage(){
-    return {
-      title: '自定义分享标题',
-      desc: '自定义分享描述',
-      path: '/pages/item?id=' + this.data.id
+    data: {
+        title: '',
+        loading: true,
+        movie: {}
+    },
+    onLoad: function(options) {
+        // 页面初始化 options为页面跳转所带来的参数
+        var _this = this;
+        app.douban.findOne(options.id).then(function(d) {
+            return _this.setData({ title: d.title, movie: d, loading: false })
+        }).catch(function(e) {
+            _this.setData({ title: '获取数据异常', movie: {}, loading: false });
+            console.error(e);
+        })
+    },
+    onReady: function() {
+        // 页面渲染完成
+        wx.setNavigationBarTitle({
+            title: this.data.title + ' < 电影 < 豆瓣'
+        })
+    },
+    onShow: function() {
+        // 页面显示
+    },
+    onHide: function() {
+        // 页面隐藏
+    },
+    onUnload: function() {
+        // 页面关闭
+    },
+    onShareAppMessage: function onShareAppMessage() {
+        return {
+            title: '自定义分享标题',
+            desc: '自定义分享描述',
+            path: '/pages/item?id=' + this.data.id
+        }
     }
-  }
 })
 
 
